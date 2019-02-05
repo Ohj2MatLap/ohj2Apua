0bf95602bd136a06cdbbd7280a6d3a923e562296
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2019-02-05 22:59:15 +0200

    Testaa lukujen järjestystä

--- container/main.cpp
+++ container/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 
 void read_integers(std::vector< int >& ints, int count)
@@ -10,10 +11,64 @@ void read_integers(std::vector< int >& ints, int count)
     {
         std::cin >> new_integer;
         // TODO: Implement your solution here
+        ints.push_back(new_integer);
     }
 }
 
 // TODO: Implement your solution here
+bool same_values(std::vector<int> integers){
+    //Palauttaa arvon true, jos kaikki arvot ovat samoja
+    //Huom! #include <algorithm>
+    if (std::adjacent_find(integers.begin(), integers.end(), std::not_equal_to<int>() ) == integers.end() )
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool is_ordered_non_strict_ascending(std::vector<int> integers){
+    //Palauttaa arvon true, jos arvot ovat kasvavassa järjestyksessä
+    int b = integers.at(0);
+    for(int i : integers){
+        if(i<b){return false;}
+        else{b=i;}
+    }
+    return true;
+}
+bool is_arithmetic_series(std::vector<int> integers){
+    //Palauttaa arvon true, jos kaikien arvojen kahden peräkkäisen arvon erotus on vakio
+    bool first = true;
+    int vakio = integers.at(0)-integers.at(1);
+    int edeltava_arvo = integers.at(0);
+    for(int i : integers){
+        if (first)
+            { first = false; continue; }
+        if (edeltava_arvo-i==vakio){
+            edeltava_arvo=i;
+            continue;}
+        else{return false;}
+    }
+    return true;
+}
+
+bool is_geometric_series(std::vector<int> integers){
+    //Palauttaa arvon true, jos peräkkäisten arvojen osamäärä on vakio
+    bool first = true;
+    int vakio = integers.at(0)/integers.at(1);
+    int edeltava_arvo = integers.at(0);
+    for(int i : integers){
+        if (first)
+            { first = false; continue; }
+        if (edeltava_arvo/i==vakio){
+            edeltava_arvo=i;
+            continue;}
+        else{return false;}
+    }
+    return true;
+}
+
 
 
 int main()
@@ -31,10 +86,10 @@ int main()
     else
         std::cout << "All the integers are not the same" << std::endl;
 
-    if(is_ordered_ascending(integers))
-        std::cout << "The integers are in an ascending order" << std::endl;
+    if(is_ordered_non_strict_ascending(integers))
+        std::cout << "The integers are in a non-strict ascending order" << std::endl;
     else
-        std::cout << "The integers are not in an ascending order" << std::endl;
+        std::cout << "The integers are not in a non-strict ascending order" << std::endl;
 
     if(is_arithmetic_series(integers))
         std::cout << "The integers form an arithmetic series" << std::endl;
