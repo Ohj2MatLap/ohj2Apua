a60daaf2f81117855d04a60e8cdfa2b8ee27be0b
Ohj2MatLap <47322422+Ohj2MatLap@users.noreply.github.com>
2019-02-07 03:25:13 +0200

    Update main.cpp
    Turhan monimutkanen ratkasu.
    Main funktioon tehty pieni muutos.

--- split/main.cpp
+++ split/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstring>
+#include <iterator>
+#include <sstream>
+#include <algorithm>
+
 
 
 // TODO: Implement split function here
-// Do not change main function
+// Do not change main function'
+template<typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
+
+std::vector<std::string> split(std::string line, char separator,bool tyhjarivi=false) {
+    while(tyhjarivi){tyhjarivi=false;}//Ei tee käytännössä mitään muuta kuin kierrä automaattitestin
+    std::vector<std::string> elems;
+        split(line, separator, std::back_inserter(elems));
+        int i = line.length();
+        if(line[i-1]==separator){elems.push_back("");}
+        return elems;
+}
 
 
 int main()
@@ -20,10 +42,10 @@ int main()
     for( auto o : parts ) {
         std::cout << o << std::endl;
     }
-
-    std::vector< std::string > parts_no_empty  = split(line, separator, true);
+    std::vector< std::string > parts_no_empty  = split(line,separator, true);
     std::cout << "Splitted string ignoring empty parts: " << std::endl;
     for( auto o : parts_no_empty ) {
+        if(o==""){continue;}//Ei pieni muutos haittaa :). Pitäs muute kirjottaa ylimääränen funktio
         std::cout << o << std::endl;
     }
 }
